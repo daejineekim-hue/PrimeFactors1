c59f3b6d47794aeab05c3d12205aa2efc72cf5ad
Daejin-Kim <daejinee.kim@gmail.com>
2026-07-01 09:45:21 +0900

    초기 커밋.

---
+++ PrimeFactors/prime-factors_test.cpp
@@ -0,0 +1,11 @@
+#include "gmock/gmock.h"
+#include "prime-factors.cpp"
+#include <vector>
+
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+    PrimeFactors prime_factor;
+    vector<int> expected = {};
+    EXPECT_EQ(expected, prime_factor.of(1));
+}
