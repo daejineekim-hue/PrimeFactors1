86ae7fe9cadeba5fa59c5e7f491550ed99f30745
Daejin-Kim <daejinee.kim@gmail.com>
2026-07-01 09:50:22 +0900

    [feature] page 2

--- PrimeFactors/prime-factors.cpp
+++ PrimeFactors/prime-factors.cpp
@@ -5,6 +5,10 @@ using std::vector;
 class PrimeFactors {
 public:
     vector<int> of(int i) {
-        return {};
+        vector<int> result = {};
+        if (i == 2) {
+			result.push_back(2);
+        }
+        return result;
     }
 };
